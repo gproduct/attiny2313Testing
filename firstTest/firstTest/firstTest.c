f346aa095fc408465c6f9bfb57706bb184e61ae1
MrBurnzie <gproduct1379@yahoo.com>
2015-11-18 10:55:46 +0100

    Using 2 leds and shifting

--- firstTest/firstTest/firstTest.c
+++ firstTest/firstTest/firstTest.c
@@ -5,9 +5,10 @@
  *  Author: Sili 
  */ 
 /*
-* Objectives
-* Set F_CPU :)
-* 
+* Description
+* Using 2 leds to turn them on and off with a simple shift command
+* Working great
+*
 */
 //unsigned long long
 #define F_CPU	11059200ULL
@@ -16,12 +17,13 @@
 #include <util/delay.h>
 int main(void)
 {
-	DDRB = 0x01;
+	DDRB = 0x03;
+	
     while(1)
     {
-        PORTB = 0x01;
-		_delay_ms(1000);
-		PORTB = 0x00;
-		_delay_ms(1000);
+		PORTB = 0x01 << 1;
+		_delay_ms(500);
+		PORTB = 0x01;
+		_delay_ms(500);
     }
 }
