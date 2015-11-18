a065348f53e7010bb8829bd0b7fb582cb79ddfab
MrBurnzie <gproduct1379@yahoo.com>
2015-11-18 13:43:09 +0100

    PWM with an led, hopefully works with PWM too.

--- firstTest/firstTest/firstTest.c
+++ firstTest/firstTest/firstTest.c
@@ -6,7 +6,7 @@
  */ 
 /*
 * Objectives
-* idk
+* We are using 1 led and the second PWM channel so I could learn how to change the ports
 * 
 */
 //unsigned long long
@@ -16,9 +16,27 @@
 #include <util/delay.h>
 int main(void)
 {
+	DDRB  = 0x08;//make the pin as output
+	TCCR1A = (1 << COM1A1) | (1 << WGM00);//setup the pwm for that pin
+	OCR1A = 0x10;//put a value for it
+	TCCR1B = (1 << CS01);
 	//in branch led we have some basic led code
     while(1)
     {
-        
+		uint8_t led;//because it mustn't go over 255
+		for(led = 0;led<255;led++)
+		{
+			OCR1A = led;
+			_delay_ms(50);	
+		}
+		_delay_ms(1000);
+		OCR1A = 255;
+		for(led = 255;led > 0;led--)
+		{
+			OCR1A = led;
+			_delay_ms(50);
+		}
+		OCR1A = 0x00;
+		_delay_ms(5000);
     }
 }
