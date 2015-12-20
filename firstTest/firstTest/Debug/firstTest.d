e5cb5be5e67fe79511914a3e8f9084251a2c65f1
MrBurnzie <gproduct1379@yahoo.com>
2015-12-20 15:47:16 +0100

    Interrupt with the attiny2313

--- firstTest/firstTest/Debug/firstTest.d
+++ firstTest/firstTest/Debug/firstTest.d
@@ -12,7 +12,8 @@ firstTest.d firstTest.o: .././firstTest.c \
  c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\lock.h \
  c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\util\delay.h \
  c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
- c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\math.h
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\math.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\interrupt.h
 
 c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\io.h:
 
@@ -41,3 +42,5 @@ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-g
 c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
 
 c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\math.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
--- firstTest/firstTest/firstTest.c
+++ firstTest/firstTest/firstTest.c
@@ -14,11 +14,47 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
+
+#define DataPort PORTB
+#define DataDDR DDRB
+
+ISR(INT1_vect)								//run this if we have an external interrupt from PORTD PIN 4 (pushButton)
+{
+	unsigned char i,temp;
+	
+	_delay_ms(100);
+	temp = DataPort;
+	
+	for(i = 0; i < 5; i++)
+	{
+		DataPort = 0x00;
+		_delay_ms(500);
+		DataPort = 0xff;
+		_delay_ms(500);
+	}
+	DataPort = temp;
+}
 int main(void)
 {
-	//in branch led we have some basic led code
+	DDRD = 1 << PD4;						//setting the PORTD PIN 4 as input for push button
+	PORTD = 1 << PD4;						//pull up for the pin
+	
+	DataDDR = 0xff;							//portb as output for the leds
+	DataPort = 0x01;						//init for just one led
+	
+	GIMSK= 1 << INT1;						//turning on the interupt (GIMSK register)
+	MCUCR = 1 << ISC01 | 1 << ISC00;		//rising edge
+	
+	sei();
+	
     while(1)
     {
-        
+        if(DataPort == 0x08){				//if at the end of (1000) then start over
+			DataPort = 1;
+		}
+		else
+			DataPort = DataPort << 1;		//shift it so it will turn on the next led
+		_delay_ms(500);
     }
 }
