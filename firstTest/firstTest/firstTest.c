cf0e16ee4723de85a15c3314df346bf3e47f00a7
MrBurnzie <gproduct1379@yahoo.com>
2015-12-28 23:13:01 +0100

    just checking for the input

--- firstTest/firstTest/firstTest.c
+++ firstTest/firstTest/firstTest.c
@@ -1,42 +1,57 @@
-/*
- * firstTest.c
- *
- *  The first code that will run on my attint2313
- *  Author: Sili 
- */ 
+
+
+
 
 #define F_CPU	11059200ULL
+#define DEBOUNCE 10
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
 
+	
+		
 void setup()
 {
 	
-	DDRB = 0x04;
+	DDRB = 0x01;
 	
 	DDRD &= ~(1<<PD4); //Define input on PC3
 	PORTD |= (1<<PD4); // set pull up resistor
+	
+	GIMSK= 1 << INT1;						//turning on the interupt (GIMSK register)
+	MCUCR = 1 << ISC01 | 1 << ISC00;		//rising edge
+	
+	sei();
 }
 int main(void){
 	setup();
-		
-	int counted = 0;
+	int pressed = 0;
+	int PCL = 0;
+	int RCL = 0;
+	
 	while(1)
 	{
 		if(PIND & (1<<PD4)) {
-			counted++;
-			_delay_ms(400);
-		}
-		if(counted == 5){
-			PORTB = 0x04;
-		}
-		else if(counted == 7){
-			PORTB = 0x04;
+			PCL++;
+			if(PCL > 500)
+			{
+				if(pressed == 0)
+				{
+					PORTB = 0x01;
+					pressed = 1;
+				}
+				PCL = 0;
+			}
 		}
 		else{
-			PORTB = 0x00;
+			RCL++;
+			if(RCL > 500)
+			{
+				pressed = 0;
+				PORTB = 0x00;
+				RCL = 0;
+			}
 		}
-		_delay_ms(100);
 	}
 }
